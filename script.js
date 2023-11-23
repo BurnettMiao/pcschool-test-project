3df58f59ffa9027f7427cb8d36b0755499ddb644
BurnettMiao <searchfor1987@gmail.com>
2023-11-23 17:39:54 +0800

    fix slide bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 480,
+  slidesPerView: 1,
+  spaceBetween: 10,
   centeredSlides: true,
   loop: true,
   autoplay: {
@@ -11,20 +11,30 @@ var swiper = new Swiper('.mySwiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 20,
-  //   },
-  //   768: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 580,
-  //   },
-  //   1024: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 580,
-  //   },
-  // },
+  breakpoints: {
+    // 640: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 20,
+    // },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 580,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    1140: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
