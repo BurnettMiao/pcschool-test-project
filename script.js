3bf466557bf2ee8ba62c77f2fda905b85b923dd1
BurnettMiao <searchfor1987@gmail.com>
2023-12-18 09:07:00 +0800

    add mobile menu

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+// mobile menu section
+const body = document.querySelector('body');
+
+const hamburgerBtn = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.fa-xmark');
+const mobileMemu = document.querySelector('.mobile-menu-section');
+
+hamburgerBtn.addEventListener('click', () => {
+  mobileMemu.classList.add('active');
+  body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileMemu.classList.remove('active');
+  body.style.overflow = 'auto';
+});
+
+const mobileAllCourse = document.querySelector('.mobile-all-course');
+const mobileAllCourseLists = document.querySelector('.mobile-all-course-lists');
+const mobileAllCourseHeader = document.querySelector(
+  '.mobile-all-course-header'
+);
+
+mobileAllCourse.addEventListener('click', () => {
+  mobileAllCourseLists.classList.add('active');
+});
+
+mobileAllCourseHeader.addEventListener('click', () => {
+  mobileAllCourseLists.classList.remove('active');
+});
+
+const mobileMembers = document.querySelector('.mobile-members');
+const mobileMembersLists = document.querySelector('.mobile-members-lists');
+const mobileMembersHeader = document.querySelector('.mobile-members-header');
+
+mobileMembers.addEventListener('click', () => {
+  mobileMembersLists.classList.add('active');
+});
+
+mobileMembersHeader.addEventListener('click', () => {
+  mobileMembersLists.classList.remove('active');
+});
+
+const mobilePersonal = document.querySelector('.mobile-personal');
+const mobilePersonalLists = document.querySelector('.mobile-personal-lists');
+const mobilePersonalHeader = document.querySelector('.mobile-personal-header');
+
+mobilePersonal.addEventListener('click', () => {
+  mobilePersonalLists.classList.add('active');
+});
+
+mobilePersonalHeader.addEventListener('click', () => {
+  mobilePersonalLists.classList.remove('active');
+});
+
+window.onclick = function (event) {
+  if (event.target === mobileMemu) {
+    mobileMemu.classList.remove('active');
+  }
+};
+
+// document.addEventListener('click', () => {
+//   if (mobileMemu.contains('active')) {
+//     mobileMemu.classList.remove('active');
+//   }
+// });
+
+// Swiper section
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 10,
